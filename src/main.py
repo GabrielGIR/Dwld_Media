951d7cd94755b7829d88e1a5281cd30c811fe7be
Rush <gabrielroel777@gmail.com>
2024-12-05 16:55:03 -0300

    Prueba Jinja2 + python-multipart

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
 import os
-from fastapi import FastAPI, HTTPException, Query
-from fastapi.responses import JSONResponse
 import yt_dlp
-
+from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query, Request, Form
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+
 
 
 app = FastAPI()
@@ -13,25 +14,31 @@ app = FastAPI()
 static_path = os.path.join(os.path.dirname(__file__), 'static/')
 templates_path = os.path.join(os.path.dirname(__file__), 'templates/')
 
-app.mount('static/', StaticFiles(directory=static_path), 'static')
+app.mount('/static', StaticFiles(directory=static_path), 'static')
 
-templates=Jinja2Templates(directory=templates_path)
+templates = Jinja2Templates(directory=templates_path)
 
-@app.get('/download', response_class=JSONResponse)
-async def download_video(url: str = Query(..., description="URL del video de YouTube a descargar")):
+
+@app.get('/', tags=['home'])
+def home(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request, 'message': 'Welcome'})
+
+
+@app.post('/download', response_class=JSONResponse)
+async def download_video(url: str = Form(...)):
     # Opciones de configuración para yt_dlp
     yt_opts = {
         'format': 'bestvideo+bestaudio/best',  # Requiere FFMPEG
         'outtmpl': './downloads/%(title)s.%(ext)s',
         'merge_output_format': 'mp4',
     }
-
     try:
         # Descargar el video usando yt_dlp
         with yt_dlp.YoutubeDL(yt_opts) as ydl:
             ydl.download([url])
-        
+
         return {"message": "Descarga completada", "url": url}
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al descargar el video: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Error al descargar el video: {str(e)}")
